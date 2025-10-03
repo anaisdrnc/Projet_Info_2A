a6814c9589bab0b7b1352c0a3a9508b0e7af9a03
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-10-03 10:36:09 +0000

    test_User

---
+++ tests/Model/test_User.py
@@ -0,0 +1,32 @@
+import pytest
+from pydantic_core import ValidationError
+
+
+from src.Model.User import User
+
+
+def test_user_constructor_ok():
+    User1 = User(
+        id=1, username="Lil", password="1234password", firstname="Lilas", lastname="Dupont", email="lilas.dpt@gmail.com"
+    )
+    assert User1.id == 1
+    assert User1.username == "Lil"
+    assert User1.password == "1234password"
+    assert User1.password == "Lilas"
+    assert User1.password == "Dupont"
+    assert User1.email == "lilas.dpt@gmail.com"
+
+
+def test_user_constructor_throws_on_incorrect_input():
+    with pytest.raises(ValidationError) as exception_info:
+        User(
+            id='one',
+            username="Lil",
+            password="1234password",
+            firstname="Lilas",
+            lastname="Dupont",
+            email="lilas.dpt@gmail.com",
+        )
+    assert "id" in str(
+        exception_info.value
+    ) and "Input should be a valid integer, unable to parse string as an integer" in str(exception_info.value)
