a7a3c6abb5abd27c67f9fd4475f9496d120335bd
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-10-28 17:01:00 +0000

    tests user

--- tests/Model/test_User.py
+++ tests/Model/test_User.py
@@ -1,10 +1,5 @@
-import sys
-
 import pytest
 from pydantic_core import ValidationError
-
-sys.path.insert(0, "~work/Projet_Info_2A/src/Model")
-import unittest
 
 from src.Model.User import User
 
@@ -21,7 +16,7 @@ def test_user_constructor_ok():
     assert User1.email == "lilas.dpt@gmail.com"
 
 
-def test_user_constructor_throws_on_incorrect_input():
+def test_user_constructor_throws_on_incorrect_id():
     with pytest.raises(ValidationError) as exception_info:
         User(
             id="one",
@@ -34,3 +29,59 @@ def test_user_constructor_throws_on_incorrect_input():
     assert "id" in str(
         exception_info.value
     ) and "Input should be a valid integer, unable to parse string as an integer" in str(exception_info.value)
+
+def test_user_constructor_throws_on_incorrect_username():
+    with pytest.raises(ValidationError) as exception_info:
+        User(
+            id=1,
+            username=123,
+            password="1234password",
+            firstname="Lilas",
+            lastname="Dupont",
+            email="lilas.dpt@gmail.com",
+        )
+    assert "username" in str(
+        exception_info.value
+    ) and "Input should be a valid string" in str(exception_info.value)
+
+def test_user_constructor_throws_on_incorrect_firstname():
+    with pytest.raises(ValidationError) as exception_info:
+        User(
+            id=1,
+            username='Lil',
+            password="1234password",
+            firstname=123,
+            lastname="Dupont",
+            email="lilas.dpt@gmail.com",
+        )
+    assert "firstname" in str(
+        exception_info.value
+    ) and "Input should be a valid string" in str(exception_info.value)
+
+def test_user_constructor_throws_on_incorrect_lastname():
+    with pytest.raises(ValidationError) as exception_info:
+        User(
+            id=1,
+            username='Lil',
+            password="1234password",
+            firstname='Lilas',
+            lastname=True,
+            email="lilas.dpt@gmail.com",
+        )
+    assert "lastname" in str(
+        exception_info.value
+    ) and "Input should be a valid string" in str(exception_info.value)
+
+def test_user_constructor_throws_on_incorrect_email():
+    with pytest.raises(ValidationError) as exception_info:
+        User(
+            id=1,
+            username='Lil',
+            password="1234password",
+            firstname='Lilas',
+            lastname='Dupont',
+            email=[1, 2],
+        )
+    assert "email" in str(
+        exception_info.value
+    ) and "Input should be a valid string" in str(exception_info.value)
