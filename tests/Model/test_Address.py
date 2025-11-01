4ecc3a22cac4ab54404d5ad2a1bc5c1f17edcba8
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-11-01 18:24:43 +0000

    test

--- tests/Model/test_Address.py
+++ tests/Model/test_Address.py
@@ -3,33 +3,27 @@ from pydantic_core import ValidationError
 
 from src.Model.Address import Address
 
+
 def test_address_constructor_ok():
-    Address1 = Address(
-        address = "51 rue Blaise Pascal",
-        postalcode = 35170,
-        city = "Bruz"
-    )
+    Address1 = Address(address="51 rue Blaise Pascal", postalcode=35170, city="Bruz")
     assert Address1.address == "51 rue Blaise Pascal"
     assert Address1.postalcode == 35170
     assert Address1.city == "Bruz"
 
+
 def test_address_constructor_throws_on_incorrect_address():
     with pytest.raises(ValidationError) as exception_info:
-        Address(address = 51 , postalcode = 35170, city = "Bruz")
-    assert "address" in str(
-        exception_info.value
-    ) and "Input should be a valid string" in str(exception_info.value)
+        Address(address=51, postalcode=35170, city="Bruz")
+    assert "address" in str(exception_info.value) and "Input should be a valid string" in str(exception_info.value)
+
 
 def test_address_constructor_throws_on_incorrect_postalcode():
     with pytest.raises(ValidationError) as exception_info:
-        Address(address = "51 rue Blaise Pascal", postalcode = "Trente cinq", city = "Bruz")
-    assert "postalcode" in str(
-        exception_info.value
-    ) and "Input should be a valid integer" in str(exception_info.value)
+        Address(address="51 rue Blaise Pascal", postalcode="Trente cinq", city="Bruz")
+    assert "postalcode" in str(exception_info.value) and "Input should be a valid integer" in str(exception_info.value)
+
 
 def test_address_constructor_throws_on_incorrect_city():
     with pytest.raises(ValidationError) as exception_info:
-        Address(address = "51 rue Blaise Pascal", postalcode = 35170, city = True)
-    assert "city" in str(
-        exception_info.value
-    ) and "Input should be a valid string" in str(exception_info.value)
+        Address(address="51 rue Blaise Pascal", postalcode=35170, city=True)
+    assert "city" in str(exception_info.value) and "Input should be a valid string" in str(exception_info.value)
