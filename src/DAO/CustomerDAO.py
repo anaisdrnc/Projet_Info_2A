84d03abb17706478e1f69f2f791bdf0bf8970534
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-11-02 20:18:58 +0000

    customerDAO suite

--- src/DAO/CustomerDAO.py
+++ src/DAO/CustomerDAO.py
@@ -1,11 +1,10 @@
 from DBConnector import DBConnector
-from src.Model.User import User
-from src.Model.Customer import Customer
+#from src.Model.User import User
+#from src.Model.Customer import Customer
 
-class CustomerDAO():
-    def add_customer(
-        self
-    ):
+
+class CustomerDAO:
+    def add_customer(self):
         with DBConnector().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
@@ -13,11 +12,14 @@ class CustomerDAO():
                     "VALUES "
                     "(%(first_name)s, %(last_name)s, %(user_name)s, %(password)s, %(email)s)"
                     "RETURNING id_user;",
-                    {"last_name" : self.lastname,
-                    "first_name" : self.firstname,
-                    "user_name": self.username,
-                    "password" : self.password,
-                    "email" : self.email}
+                    {
+                        "last_name": self.lastname,
+                        "first_name": self.firstname,
+                        "user_name": self.username,
+                        "password": self.password,
+                        "email": self.email,
+                    },
                 )
                 res = cursor.fetchone()
+                
         print(res)
