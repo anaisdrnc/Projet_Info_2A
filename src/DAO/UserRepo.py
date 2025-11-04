17d23cde70ac97d56e9de855fbcba216a8660b0e
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-11-04 11:37:10 +0000

    test et user repo

--- src/DAO/UserRepo.py
+++ src/DAO/UserRepo.py
@@ -6,38 +6,39 @@ from utils.singleton import Singleton
 from src.Model.User import User
 from .DBConnector import DBConnector
 
-class UserRepo(metaclass = Singleton):
+
+class UserRepo():
     """DAO User"""
 
     def __init__(self):
         """Initialize a new UserDAO instance with a database connector."""
         self.db_connector = DBConnector()
-    
+
     def add_user(self, user):
-        """add user to the database and return id_user, in order to add the user to the 
+        """add user to the database and return id_user, in order to add the user to the
         proprer table (customer, driver or admin) with another method"""
         res = None
         try:
             res = self.db_connector.sql_query(
-                    "INSERT INTO users (first_name, last_name, user_name, password, email)"
-                    "VALUES "
-                    "(%(first_name)s, %(last_name)s, %(user_name)s, %(password)s, %(email)s)"
-                    "RETURNING id_user;",
-                    {
-                        "last_name": user.lastname,
-                        "first_name": user.firstname,
-                        "user_name": user.username,
-                        "password": user.password,
-                        "email": user.email,
-                    },
-                )
+                "INSERT INTO users (first_name, last_name, user_name, password, email)"
+                "VALUES "
+                "(%(first_name)s, %(last_name)s, %(user_name)s, %(password)s, %(email)s)"
+                "RETURNING id_user;",
+                {
+                    "last_name": user.lastname,
+                    "first_name": user.firstname,
+                    "user_name": user.username,
+                    "password": user.password,
+                    "email": user.email,
+                },
+            )
         except Exception as e:
             logging.info(e)
         if res:
             user.id = res["id_user"]
             return user.id
         return None
-    
+
     def get_by_id(self, user_id: int) -> Optional[User]:
         raw_user = self.db_connector.sql_query("SELECT * from users WHERE id=%s", [user_id], "one")
         if raw_user is None:
@@ -59,6 +60,13 @@ class UserRepo(metaclass = Singleton):
             return None
         list_users = []
         for line in raw_list:
-            user = User(id = line['id_user'],firstname = line['firstname'],lastname = line['lastname'],username = line['username'], password = line['password'], email = line['email'])
+            user = User(
+                id=line["id_user"],
+                firstname=line["firstname"],
+                lastname=line["lastname"],
+                username=line["username"],
+                password=line["password"],
+                email=line["email"],
+            )
             list_users.append(user)
         return list_users
