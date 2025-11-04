a10a50ba226287619ffd4b43b760a0425d88d597
GIRARD Robin <robin.girard003@gmail.com>
2025-11-04 16:35:28 +0100

    upgrade maps

--- __main__.py
+++ __main__.py
@@ -1,4 +1,6 @@
-from src.App.API import run_app
+#from src.App.API import run_app
+from src.Service.Google_Maps import carte_api
 
 if __name__ == "__main__":
-    app = run_app()
+    #app = run_app()
+    carte_api.main()
