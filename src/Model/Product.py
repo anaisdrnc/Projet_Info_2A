40403c9e185292562747c6c235f1587864ffd370
id2587 <juliette.legendre@eleve.ensai.fr>
2025-11-04 16:05:13 +0000

    modif

--- src/Model/Product.py
+++ src/Model/Product.py
@@ -4,10 +4,10 @@ from pydantic import BaseModel, Field
 
 
 class Product(BaseModel):
-    id: int
+    id_product: int
     name: str
     price: float = Field(..., gt=0, description="The selling price must be strictly positive")
     production_cost: float = Field(..., gt=0, description="The production cost must be strictly positive")
     product_type: Literal["drink", "lunch", "dessert"]
     description: str
-    stock_quantity: int = Field(..., ge=0, description="The stock quantity must be strictly positive or equals to zero")
+    stock: int = Field(..., ge=0, description="The stock quantity must be strictly positive or equals to zero")
