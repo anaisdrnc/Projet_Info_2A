0aaacbe0ba9c19c2a04777421ed0cc9c0982305f
anaisdrnc <anais.duranceau@eleve.ensai.fr>
2025-11-06 13:46:29 +0000

    admindao et test

--- src/DAO/CustomerDAO.py
+++ src/DAO/CustomerDAO.py
@@ -1,33 +1,39 @@
-from src.DAO.DBConnector import DBConnector
 import logging
-from src.Model.User import User
-from src.Model.Customer import Customer
+from src.DAO.DBConnector import DBConnector
 from src.DAO.UserRepo import UserRepo
+from src.Model.Customer import Customer
+from utils.securite import hash_password
 
 
 class CustomerDAO:
-    """Connect with the customer table
-    add customer,"""
+    """DAO pour gérer les admins dans la base de données."""
 
-    def __init__(self, db_connector):
-        """Initialize a new CustomerDAO instance with a database connector."""
-        self.db_connector = DBConnector()
+    def __init__(self, db_connector=None):
+        """Initialise CustomerDAO avec un connecteur DB."""
+        self.db_connector = db_connector or DBConnector()
+        self.user_repo = UserRepo(self.db_connector)
 
-    def add_customer(self, user: User):
-        """Add a customer to the database (from a user, creating the users in the user table
-        and then putting the customer in the customers database with the id_user)"""
-        user_repo = UserRepo(self.db_connector)
-        id_user = user_repo.add_user(user)
+    def add_customer(self, customer: Customer):
+        """
+        Ajoute un customer dans la base de données.
+        1) Crée l'utilisateur dans la table users.
+        2) Ajoute le customer dans la table customer.
+        """
+        id_user = self.user_repo.add_user(customer)
+        if not id_user:
+            return False
+
         try:
             res = self.db_connector.sql_query(
-                "INSERT INTO customer (id_user)"
-                "VALUES (%(id_user)s)"
-                "RETURNING id_customer;",
+                "INSERT INTO customer (id_user) VALUES (%(id_user)s) RETURNING id_customer;",
                 {"id_user": id_user},
             )
         except Exception as e:
             logging.info(e)
-        if res is not None:
-            id_customer = res["id_customer"]
-            return id_customer
-        return None
+            return False
+
+        if res and "id_customer" in res:
+            customer.id_customer = res["id_customer"]
+            return True
+
+        return False
--- src/Model/Customer.py
+++ src/Model/Customer.py
@@ -6,3 +6,4 @@ from src.Model.User import User
 
 class Customer(User):
     address: Optional[Address] = None
+    id_customer: int | None = None
