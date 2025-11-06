8e275b8fc949aa01bbe41e05ff268f8b26e398a8
quentinbibi <quentin.billaudaz@eleve.ensai.fr>
2025-11-06 10:49:55 +0000

    push gentil

--- src/App/API.py
+++ src/App/API.py
@@ -2,13 +2,13 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 #from .MovieController import movie_router
-from .UserController import user_router
+#from .UserController import user_router
 from .ProductController import product_router
 from .OrderController import order_router
 from .AdministratorController import admin_router
 
 app = FastAPI(title="Projet Info 2A", description="UB'EJR")
-app.include_router(user_router)
+#app.include_router(user_router)
 #app.include_router(movie_router)
 app.include_router(product_router)
 app.include_router(order_router)
