89a5a032a23753fa058c8d7d5e220c22cc9b65d7
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-11-13 09:54:14 +0000

    CLI

--- src/CLI/place_order_view.py
+++ src/CLI/place_order_view.py
@@ -24,7 +24,7 @@ class PlaceOrderView(VueAbstraite):
         address_service = AddressService(addressdao)
 
         #get id_user for creating order
-        id_customer = 3
+        id_customer = Session().get_id_user()
 
         #get the list of products, the quantities and the price of the order
         list_choosen_products_names = []
