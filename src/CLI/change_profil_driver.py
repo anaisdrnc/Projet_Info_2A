f10fac2657ba49120a5a31799b729e85e7d612ef
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-11-14 15:54:58 +0000

    view change profil

--- src/CLI/change_profil_driver.py
+++ src/CLI/change_profil_driver.py
@@ -15,40 +15,53 @@ from src.Service.DriverService import DriverService
 from src.Service.UserService import UserService
 
 class ChangeProfilDriver(VueAbstraite):
-    """view used to change the profil of the drivers, specially their password (since it is the admin
-    that create the driver) and the mean of transport"""
+    def change_profil_driver(self):
+        """view used to change the profil of the drivers, specially their password (since it is the admin
+        that create the driver) and the mean of transport"""
 
-    driverdao = DriverDAO(DBConnector(test = False))
-    user_repo = UserRepo(DBConnector(test = False))
-    driverservice = DriverService(driverdao)
-    userservice = UserService(user_repo)
+        driverdao = DriverDAO(DBConnector(test = False))
+        user_repo = UserRepo(DBConnector(test = False))
+        driverservice = DriverService(driverdao)
+        userservice = UserService(user_repo)
 
-    choice = inquirer.select(
-            message="Choose : ",
-            choices=[
-                "Change password",
-                "Change the mean of transport",
-                "Go back",
-            ],
-        ).execute()
+        choice = inquirer.select(
+                message="Choose : ",
+                choices=[
+                    "Change password",
+                    "Change the mean of transport",
+                    "Go back",
+                ],
+            ).execute()
 
-    match choice:
-        case "Go back":
-            return MenuDriver()
+        match choice:
+            case "Go back":
+                return MenuDriver()
 
-        case "Change password":
-            username = Session().username
-            old_password = inquirer.secret(message="Enter your current password : ").execute()
-            new_password = inquirer.secret(message= "Enter your new password :").execute()
-            works = userservice.change_password(user_name=username, old_password=old_password, new_password=new_password)
+            case "Change password":
+                username = Session().username
+                old_password = inquirer.secret(message="Enter your current password : ").execute()
+                new_password = inquirer.secret(message= "Enter your new password :").execute()
+                works = userservice.change_password(user_name=username, old_password=old_password, new_password=new_password)
 
-            if works :
-                message = "Success"
-                return MenuDriver(message)
+                if works :
+                    message = "Success"
+                    return MenuDriver(message)
             
-            else :
-                message = "The operation didn't work, please try again."
-                return MenuDriver(message)
+                else :
+                    message = "The operation didn't work, please try again."
+                    return MenuDriver(message)
         
-        case "Change the mean of transport":
+            case "Change the mean of transport":
+                username = Session().username
+                new_locomotion = inquirer.select(
+                    message="Choose your new mean of transport :",
+                    choices= ["Bike", "Car"]
+                )
+                works = driverservice.update_driver(username, new_locomotion)
+                if works:
+                    message = "Your mean of transport has been successfully changed."
+                    return MenuDriver(message)
             
+                else : 
+                    message = "The operation didnt work, please try again."
+                    return MenuDriver(message)
