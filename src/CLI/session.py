d070be58ed51ce73da1882cda23b03e915885dc8
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-11-14 09:51:16 +0000

    CLI + customerDAO

--- src/CLI/session.py
+++ src/CLI/session.py
@@ -11,21 +11,21 @@ class Session:
     def __init__(self):
         """Création de la session"""
         self.username = None
-        self.id_user = None
+        self.id_customer = None
         self.debut_connexion = None
 
-    def connexion(self, customer):
+    def connexion(self, user, id_customer):
         """Enregistement des données en session"""
-        self.username = customer.user_name
-        self.id_user = customer.id
+        self.username = user.user_name
+        self.id_customer = id_customer
         self.debut_connexion = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
     def deconnexion(self):
         """Suppression des données de la session"""
         self.username = None
-        self.id_user = None
+        self.id_customer = None
         self.debut_connexion = None
 
-    def get_id_user(self):
+    def get_id_customer(self):
         """return the id of the connected user"""
-        return self.id_user
+        return self.id_customer
