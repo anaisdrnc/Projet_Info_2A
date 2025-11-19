390a414fab8ff0a66a63ac44db9874f685183fd4
marieckebv <mariecke.baudru-vandenberghe@eleve.ensai.fr>
2025-11-19 17:59:53 +0000

    déplacement utils dans src

--- src/main_CLI.py
+++ src/main_CLI.py
@@ -3,7 +3,7 @@ import logging
 import dotenv
 
 from CLI.opening.openingview import OpeningView
-from utils.log_init import initialiser_logs
+from src.utils.log_init import initialiser_logs
 
 if __name__ == "__main__":
     # On charge les variables d'envionnement
