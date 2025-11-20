c8b213db9bc8174f7124ce736794e99f1093bead
robingirard <robin.girard@eleve.ensai.fr>
2025-11-20 11:53:33 +0000

    service

--- src/Model/Admin.py
+++ src/Model/Admin.py
@@ -1,4 +1,10 @@
-from src.Model.User import User
+import os
+import sys
+current_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(current_dir)
+sys.path.insert(0, project_root)
+
+from Model.User import User
 
 
 class Admin(User):
